846b9f116b81da9bcd17953f5bfb7096ab8a75dd
Ilbert130 <ilbertca27@gmail.com>
2023-06-12 13:54:50 -0400

    roleverfication created

--- middlewares/validate-roles.js
+++ middlewares/validate-roles.js
@@ -1,29 +1,40 @@
 import { request, response } from "express"
-
-
+import Role from '../models/role.js';
 
 
 export const roleVerification = (...roles) => {
 
     //This function return a middleware
-    // return async(req=request, res=response, next) => {
+    return async(req=request, res=response, next) => {
 
-    //     let hasRoles;
+        let hasRoles;
 
-    //     //Verifying if the req has the user's information
-    //     if(!req.user){
-    //         return res.status(500).json({
-    //             msg: 'It is necesary to validate the token at first and then validate the role'
-    //         });
-    //     }
+        //Verifying if the req has the user's information
+        if(!req.user){
+            return res.status(500).json({
+                msg: 'It is necesary to validate the token at first and then validate the role'
+            });
+        }
 
-    //     //Verifying role
-    //     const roleIds = [...req.user.role];
+        //Verifying role
+        const roleIds = [...req.user.role];
 
-    //     for(let i = 0; i<roleIds.length; i++){
+        for(let i = 0; i<roleIds.length; i++){
 
-    //         const id = roleIds[i].toString();
-    //         const role = await Role.find
-    //     }
-    // }
+            const id = roleIds[i].toString();
+            const role = await Role.findById(id);
+            hasRoles = roles.includes(role.role);
+
+            if(hasRoles){
+                break;
+            }
+        }
+
+        if(!hasRoles){
+            return res.status(401).json({
+                msg:`This service require one of these roles ${roles}`
+            });
+        }
+        next();
+    }
 }
